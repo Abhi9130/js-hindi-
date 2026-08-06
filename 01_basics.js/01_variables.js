38a90d52dc3a34fb7fa8f265865dda0d6231dca6
Abhi9130 <somaseabhishek@gmail.com>
2026-08-06 11:57:46 +0000

    01_variables

---
+++ 01_basics.js/01_variables.js
@@ -0,0 +1,6 @@
+console.log("abhsihek");
+//let age = 18 numbers value 
+//let isloggedIn = false boleean type 
+console.log("type of age ");
+console.log("type of undifined");//undifined
+console.log("type of null");// null 
