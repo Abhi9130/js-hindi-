73e364a47e1a485bb2f8c3c2ec23751580d00b0f
Abhi9130 <somaseabhishek@gmail.com>
2026-08-06 14:28:00 +0000

    conversion and datatypes.js

---
+++ 01_basics.js/02_conversion.js
@@ -0,0 +1,44 @@
+//letscore = "33"
+//console.log("type of score");
+// valueInNumber = Number (score);
+//console.log"("type of valueNumber");
+//console.log("type of (score"));
+//console.log (typeof score );
+//console.log ( typeof(score));
+
+// let city 
+// console.log(typeof city)
+
+
+
+//let score = "33"
+//console. log(typeof score)
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+
+
+//let score = "33"
+//.log (score)
+//console.log(typeof score)
+// let score = "33"
+// let valueInNumber = Number(score)
+// console.log(valueInNumber)
+
+
+// console.log(typeof valueInNumber)
+
+// let score = "33 abc"
+// let valueInNumber = Number(score)
+
+// console.log(typeof number)
+
+// console.log (valueInNumber)
+
+
+
+let score = true
+
+let valueInNumber = Number (score)
+
+console.log (valueInNumber)
+console.log (typeof valueInNumber)
