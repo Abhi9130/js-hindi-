d4d50d6642606a35dfd70ec436f65aaafcac66b6
Abhi9130 <somaseabhishek@gmail.com>
2026-08-07 12:26:17 +0000

    01_arrays js

---
+++ 02_basics.js/01_array.js
@@ -0,0 +1,17 @@
+// const myArr = [10, 20, 30, 40, 50];
+// console.log(myArr[0]);
+// //====output=10;=====
+// console.log(myArr[3]);
+// // arrays create krne ====
+
+
+// const arr = [10 , 20, 30]
+// arr.push(40)
+// arr.pop()
+// arr.unshift(5)
+// arr.shift()
+// console.log(arr)
+const arr = [1, 2, 3]
+// arr.pop()
+console.log(arr.includes(20))
+console.log(arr.indexOf(30))
