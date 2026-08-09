d97331b768f62ca5a1b90959693e6adb14ee38b5
Abhi9130 <somaseabhishek@gmail.com>
2026-08-09 15:46:47 +0000

    switch case default

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,16 @@
+// const userEmail = "0"
+// if(userEmail){
+//     console.log("Got user email")
+// }else{
+//     console.log("don't have user email ")
+// }
+
+
+const value = {}
+
+if (value ){
+    console.log ("yes")
+}else{
+    console.log("no")
+
+}
