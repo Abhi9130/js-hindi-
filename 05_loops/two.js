b079dd33f044f9c992b9f52640e0dded3d6c691f
Abhi9130 <somaseabhishek@gmail.com>
2026-08-13 10:25:23 +0000

    while loops

---
+++ 05_loops/two.js
@@ -0,0 +1,21 @@
+// // while loops
+// // let index = 1 
+// // while(index <= 5 ){
+// //     console.log(index)
+// //     index++
+// // }
+// //  do while loops
+
+// // let score = 1 
+// // do{
+// //     console.log(score)
+// //     score++
+// // } while(score <= 5)
+// //-->
+// do while 
+// while 
+// while do while 
+
+
+
+// //..>
