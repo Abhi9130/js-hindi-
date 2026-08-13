912e4fe225933735ffd86614785ce1ddb21bf64d
Abhi9130 <somaseabhishek@gmail.com>
2026-08-13 09:54:06 +0000

    for loops

---
+++ 05_loops/one.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+// // for(let i = 1 ;i<=2; i++){
+// //     for(let j=1; j<=2; j++){
+// //         console.log(i*j)
+// //     }
+// // }
+
+// // for(let i=1 ; i<=3; i++){
+// //     console.log("Table:", i )
+// //     for(let j= 1 ; j<=3; j++){
+// //         console.log(i*j)
+
+// //     }
+// // }
+
+// // break 
+// for(let i= 1; i<=5; i++){
+//     if(i==4){
+//         break 
+
+
+//     }
+
+//     console.log(i)
+// }
+
+// // for loop 
+// <=/<
+// i++/i--
+// nested for loop
+// Multiplication table 
+// break 
+// continue
